6c4fdd7700dd72953377b5f9b6d9f374203f7cb7
JurgenBosmans <jurgen@2004.be>
2015-02-20 11:40:46 +0000

    fdsfs

--- app/controllers/notities_controller.rb
+++ app/controllers/notities_controller.rb
@@ -2,6 +2,13 @@ class NotitiesController < ApplicationController
     before_filter :authenticate_user!
     
     def index
-        @notities = current_user.notities.all
+        case
+            when params[:belangrijk]
+                @notities = current_user.notities.where('belangrijk = ?', true) 
+            when params[:recent]
+                @notities = current_user.notities.order("created_at DESC").limit(6)
+            else
+                @notities = current_user.notities.all    
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ JbappV2::Application.routes.draw do
     root "pages#home"    
     get "home", to: "pages#home", as: "home"
     get "inside", to: "pages#inside", as: "inside"
-  
+
     
     devise_for :users
   
