f20f365c4050b2e5ef238fd3faef7f0fc9b7e845
JurgenBosmans <jurgen@2004.be>
2015-02-20 19:28:18 +0000

    lmkdsmflks

--- app/controllers/notities_controller.rb
+++ app/controllers/notities_controller.rb
@@ -1,5 +1,6 @@
 class NotitiesController < ApplicationController
     before_filter :authenticate_user!
+    before_action :set_notitie, only: [:show, :edit, :update, :destroy]
     
     def index
         case
@@ -7,8 +8,64 @@ class NotitiesController < ApplicationController
                 @notities = current_user.notities.where('belangrijk = ?', true)           
             when params[:recent]
                 @notities = current_user.notities.order("created_at DESC").limit(6)
+            when params[:alle]
+                @notities = current_user.notities.all.order("id DESC")   
             else
-                @notities = current_user.notities.all    
+                @notities = current_user.notities.all.order("id DESC").limit(6)    
         end
     end
+    
+    def show
+    end
+
+    def new
+        @notitie = Notitie.new
+    end
+
+    def edit
+    end
+
+    def create  
+        @notitie = current_user.notities.new(notitie_params)  
+        respond_to do |format|
+          if @notitie.save
+              format.html { redirect_to notities_path, notice: 'Notitie weggeschreven' }
+            format.json { render action: 'show', status: :created, location: @notitie }
+          else
+            format.html { render action: 'new' }
+            format.json { render json: @notitie.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+
+  def update
+    respond_to do |format|
+        if @notitie.update(notitie_params)
+            format.html { redirect_to notities_path, notice: 'Notitie geüpdate' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+            format.json { render json: @notitie.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @notitie.destroy
+    respond_to do |format|
+        format.html { redirect_to notities_url, notice: 'Notitie verwijderd' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_notitie
+      @notitie = Notitie.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def notitie_params
+        params.require(:notitie).permit(:notitie, :belangrijk, :user_id)
+    end   
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,6 +10,9 @@ class PagesController < ApplicationController
     @website_aantal=current_user.websites.size
     @website=current_user.websites.last
     @websites_pop=current_user.websites.last(3)
+      
+      @notitie_aantal=current_user.notities.size
+      @notitie=current_user.notities.last
   end 
     
 end
