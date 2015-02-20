e93ea8195d5c07872a7e2015591ef2e235805bd4
JurgenBosmans <jurgen@2004.be>
2015-02-20 14:03:09 +0000

    fdsfs

--- app/controllers/notities_controller.rb
+++ app/controllers/notities_controller.rb
@@ -5,6 +5,10 @@ class NotitiesController < ApplicationController
         case
             when params[:belangrijk]
                 @notities = current_user.notities.where('belangrijk = ?', true) 
+            when params[:belangrijk2]
+                @notities = current_user.notities.where('belangrijk = ?', 1) 
+            when params[:belangrijk3]
+                @notities = current_user.notities.where('belangrijk = ?', -1)             
             when params[:recent]
                 @notities = current_user.notities.order("created_at DESC").limit(6)
             else
