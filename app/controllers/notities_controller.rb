838b196dc59b69ee62320ea2ea32a23c3cfddbc7
JurgenBosmans <jurgen@2004.be>
2015-02-20 14:31:04 +0000

    fdsfs

--- app/controllers/notities_controller.rb
+++ app/controllers/notities_controller.rb
@@ -4,11 +4,7 @@ class NotitiesController < ApplicationController
     def index
         case
             when params[:belangrijk]
-                @notities = current_user.notities.where('belangrijk = ?', true) 
-            when params[:belangrijk2]
-                @notities = current_user.notities.where('belangrijk = ?', 1) 
-            when params[:belangrijk3]
-                @notities = current_user.notities.where('belangrijk = ?', -1)             
+                @notities = current_user.notities.where('belangrijk = ?', true)           
             when params[:recent]
                 @notities = current_user.notities.order("created_at DESC").limit(6)
             else
