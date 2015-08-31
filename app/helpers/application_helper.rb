4009f59e20944bdefc37267f1c37cdd7fdc2b9fe
JurgenBosmans <jurgen@2004.be>
2015-08-31 17:46:09 +0000

    hyperlink

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,8 @@ module ApplicationHelper
     content_tag :i, nil, class: names.map{|name| "icon-#{name.to_s.gsub('_','-')}" }
   end
 
+ def hyperlink(text)
+    return text.gsub(/((http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?)/, '<a target="blank" href=\'\1\'>\1</a>').html_safe
+  end
+
 end
