c490dc63478251b75a27ace827bad55e3bf7fe06
JurgenBosmans <jurgen@2004.be>
2015-09-29 17:57:04 +0000

    note

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,4 +3,8 @@ class Todo < ActiveRecord::Base
   belongs_to :user
   
   validates :naam, :beschrijving, :presence => true
+	
+	# Include default devise modules. Others available are:
+  	# :confirmable, :lockable, :timeoutable and :omniauthable
+	devise :database_authenticatable
 end
--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,6 +1,10 @@
 class TodoItem < ActiveRecord::Base
-  belongs_to :todo
+ belongs_to :todo
 
+	  	# Include default devise modules. Others available are:
+  	# :confirmable, :lockable, :timeoutable and :omniauthable
+	devise :database_authenticatable
+	
   def afgewerkt?
     !afgewerkt.blank?
   end
