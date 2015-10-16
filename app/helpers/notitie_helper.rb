d9f6d606ddfe964e559fa36fe67cfb9cbba33ff9
JurgenBosmans <jurgen@2004.be>
2015-10-16 11:03:53 +0000

    dddfdf

--- app/helpers/notitie_helper.rb
+++ app/helpers/notitie_helper.rb
@@ -1,2 +1,5 @@
 module NotitieHelper
+	def hyperlink(text)
+    	return text.gsub(/((http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?)/, '<a target="_blank" href=\'\1\'>\1</a>').html_safe
+  end
 end
