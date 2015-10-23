2f5aa3e1d19eb81180d1da323ee0d7e74db84030
JurgenBosmans <jurgen@2004.be>
2015-10-23 16:18:12 +0000

    recept

--- app/models/werkwijze.rb
+++ app/models/werkwijze.rb
@@ -2,4 +2,6 @@ class Werkwijze < ActiveRecord::Base
   	belongs_to :recept
 
 	devise :database_authenticatable
+
+	default_scope { order('created_at ASC') } #let op sortering later is niet meer mogelijk
 end
